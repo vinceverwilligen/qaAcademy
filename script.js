982c857fd94d863ad469c6fe192a521addd49302
Vince Verwilligen <david.vinke@hotmail.com>
2019-01-10 23:00:49 +0700

    Adding test materials

---
+++ script.js
@@ -0,0 +1,11 @@
+var newman = require('newman');
+
+newman.run(
+  {
+    collection: './collection/qaAcademy_test.postman_collection.json',
+    environment: './environment/qaAcademy.postman_environment.json',
+    // globals: './globals/qaAcademy.postman_globals.json',
+    reporters: ['cli', 'html']
+  },
+  process.exit
+);
